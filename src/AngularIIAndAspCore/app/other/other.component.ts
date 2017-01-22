4607117e980aa24872cd949e476995dcb1ce6697
bw shelby <bw shelby>
2017-01-21 20:39:48 -0600

    new files

---
+++ src/AngularIIAndAspCore/app/other/other.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-other',
+    template: `<h1>Inline Template</h1>`
+})
+export class OtherComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
+
+
